10c5fee6e3a9956274190591e9f720a2499606a2
Belghieti <48102011+Belghieti@users.noreply.github.com>
2025-07-17 23:11:40 +0200

    Update SecurityConfig.java

--- src/main/java/com/demo/backend/config/SecurityConfig.java
+++ src/main/java/com/demo/backend/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("https://cinemamongo-production.up.railway.app","http://localhost:3000","https://cinema-front-mngo.vercel.app" 
+        cfg.setAllowedOrigins(List.of("https://cinemamongo-production.up.railway.app","http://localhost:3000","https://cinema-front-mngo.vercel.app",
                                      "https://cinema-front-mngo-git-main-simons-projects-8e8f969f.vercel.app",
                                       "https://cinema-front-mngo-git-main-simons-projects-8e8f969f.vercel.app"
                                      ));
