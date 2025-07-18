17d9c7a3ae9c2ba98c5728c64380fbd07cc11b17
Belghieti <48102011+Belghieti@users.noreply.github.com>
2025-07-18 04:52:54 +0200

    Update WebSocketConfig.java

--- src/main/java/com/demo/backend/config/WebSocketConfig.java
+++ src/main/java/com/demo/backend/config/WebSocketConfig.java
@@ -8,7 +8,13 @@ import org.springframework.web.socket.config.annotation.*;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
- @Override
+  //  private final JwtChannelInterceptor jwtChannelInterceptor;
+
+  /*  public WebSocketConfig(JwtChannelInterceptor jwtChannelInterceptor) {
+        this.jwtChannelInterceptor = jwtChannelInterceptor;
+    }*/
+
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
@@ -16,17 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-       
-        // Also register without SockJS for direct WebSocket connections
-        registry.addEndpoint("/websocket")
-                .setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
-
-  /*  @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/wss").setAllowedOriginPatterns("https://cinema-front-mngo-simons-projects-8e8f969f.vercel.app").withSockJS();
-    }*/
-
 
     /*@Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
